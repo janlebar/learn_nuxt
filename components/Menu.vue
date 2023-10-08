3d4d83edf17469b970728a9da712a440264253e1
Jan_Lebar <janstefanlebar@gmail.com>
2023-10-08 17:58:36 +0200

    menu

---
+++ components/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+        <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+        <!-- Add more menu items as needed -->
+      </ul>
+    </nav>
+  </template>
+  
